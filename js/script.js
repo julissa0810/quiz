684f7b1b58e8c1c73cb6f72d0ae63b981699c29e
Julissa <juarez.julissa22@gmail.com>
2019-01-02 22:42:33 +0000

    readme, index.html, js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,35 @@
 /* global $ */
 
 $(document).ready(function() {
+    function question1(answer){ 
+        if (answer === "reading" ) { 
+            return 4;
+        }
+        else if (answer=== "coding"){ 
+            return 5 ; 
+        }
+        else if (answer === "sport") {
+            return 3; 
+        } 
+        else if ( answer === "clothing") {
+            return 2;
+        } 
+    }
+    
+    function question2(answer) { 
+        if (answer === "waffles") { 
+            return 1;
+        } 
+        else if (answer === "panckakes"){ 
+            return 5; 
+        } 
+        else if (answer === "French Toast"){
+            return 3;
+        } 
+    } 
+    
+        
+        
     $("button").click(function() {
         var name = $("#name").val();
         var finalAnswer = "TBD";
@@ -9,6 +38,7 @@ $(document).ready(function() {
         var q2Input = $("#question2").val();
         var q3Input = $("#question3").val();
         var totalScore;
+        
     });
 
 });
