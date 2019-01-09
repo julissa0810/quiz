97a9c0a888bcc58629f3bdf5b25ef497f176a0d4
Julissa <juarez.julissa22@gmail.com>
2019-01-09 22:43:04 +0000

    did more work day 3

--- js/script.js
+++ js/script.js
@@ -2,34 +2,37 @@
 
 $(document).ready(function() {
     function question1(answer){ 
-        if (answer === "reading" ) { 
-            return 4;
-        }
-        else if (answer=== "coding"){ 
+        
+        if (answer=== "future"){ 
             return 5 ; 
         }
-        else if (answer === "sport") {
+        else if (answer === "past") {
             return 3; 
         } 
-        else if ( answer === "clothing") {
-            return 2;
-        } 
-    }
-    
+    } 
     function question2(answer) { 
-        if (answer === "waffles") { 
-            return 1;
+        if (answer === "virtual reality") { 
+            return 7;
         } 
-        else if (answer === "panckakes"){ 
-            return 5; 
-        } 
-        else if (answer === "French Toast"){
-            return 3;
+        else if (answer === "real life"){ 
+            return 2; 
         } 
     } 
     
+    function question3(answer) { 
+        if (answer === "instrumetal") {
+            return 3;
+            
+            
+        else if (answer === "jazz") { 
+            return 6; 
+        }
         
         
+        
+        
+        
+    } 
     $("button").click(function() {
         var name = $("#name").val();
         var finalAnswer = "TBD";
@@ -39,6 +42,15 @@ $(document).ready(function() {
         var q3Input = $("#question3").val();
         var totalScore;
         
+        var question1Score = question1(q1Input);
+        var question2Score = question2(q2Input); 
+    
+        
+        function totalScore () { 
+            
     });
 
 });
+            
+        
+    
